229f492db9bbb08a0675489f801c0c4f956ba035
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-07-30 21:34:05 +0100

    String arrays

--- data structures/arrays.c
+++ data structures/arrays.c
@@ -11,10 +11,17 @@ int main() {
 
     int percentages[] = {71 , 63, 52, 40, 25, 10};
     char grades[] = {'A', 'B', 'C', 'D', 'E', 'F'};
+    char *names[6];
+    names[0] = "Name1";
+    names[1] = "Name2";
+    names[2] = "Name3";
+    names[3] = "Name4";
+    names[4] = "Name5";
+    names[5] = "Name6";
 
     // display percentage matching to the grade
     for (int i = 0; i < 6; i++) {
-        printf("%d %c for an %c\n", percentages[i], 37, grades[i]);
+        printf("%s scored %d %c for an %c\n",names[i], percentages[i], 37, grades[i]);
     }
 
     return 0;
