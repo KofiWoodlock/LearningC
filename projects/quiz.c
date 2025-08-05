aba9d81ee989b6f6f0dc09e77bd7c656b63ffd3b
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-08-05 08:53:03 +0100

    Completed quiz mini game

--- projects/quiz.c
+++ projects/quiz.c
@@ -1,17 +1,20 @@
 /*
 Interactive quiz game key terms & code
+- toupper
+    - A function that takes an alphabetic char as input converts it to upper case
 */
 
 #include <stdio.h>
+#include <ctype.h>
 
 int main() {
 
     // Main game 
 
-    char questions[][80] = {"What is the capital of France?",
-                            "What is the largest planet in the solar system?",
-                            "What is the name of the first US president?",
-                            "Who painted the Mona Lisa?"};
+    char questions[][80] = {"What is the capital of France?\n",
+                            "What is the largest planet in the solar system?\n",
+                            "What is the name of the first US president?\n",
+                            "Who painted the Mona Lisa?\n"};
 
     char choices[][80] = {"A. London\nB. Brussels\nC. Paris\nD. New York",
                           "A. Neptune\nB. Jupiter\nC. Mars\nD. Saturn",
@@ -26,23 +29,25 @@ int main() {
     int score = 0;
     int i = 0;
 
-    printf("#### Quiz Game! ####");
-    while (i <= num_of_questions) {
+    printf("#### Quiz Game! ####\n");
+    while (i < num_of_questions) {
         printf("%s", questions[i]);
-        pritnf("%s", choices[i]);
-        printf("Please enter an answer: ");
-        scanf("%c", &guess);
+        printf("%s", choices[i]);
+        printf("\nPlease enter an answer: ");
+        scanf(" %c", &guess);
         
-        if (guess == answers[i]) {
-            printf("Correct!");
+        if (toupper(guess) == answers[i]) {
+            printf("Correct!\n");
             score++;
             i++;
         } else {
-            prinf("Incorrect! Try again.");
+            printf("Incorrect!\n");
+            i++;
         }
     }
 
-    printf("Quiz over");
+    printf("\n");
+    printf("Quiz over\n");
     printf("Your score was: %d", score);
 
     return 0;
