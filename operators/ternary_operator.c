65b96088303cab40a110c42bcae7fdad103da631
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-08-09 17:42:01 +0100

    Ternary operator

--- operators/ternary_operator.c
+++ operators/ternary_operator.c
@@ -4,11 +4,21 @@ Ternary operator - key code & terms
     - Shorthand syntax for if-else statements
 */
 
+#include <stdio.h>
+
 int max(int x, int y) {
     // Takes two integers and returns larger one
     return (x > y) ? x : y;
 }
 
 int main() {
-    printf("%d", max(5, 6));
+
+    // Military clock 
+    int mins = 3;
+    int hours = 15;
+    char *meridium = (hours < 12) ? "AM" : "PM";
+
+    printf("%02d:%02d %s", hours, mins, meridium);
+
+    return 0;
 }
