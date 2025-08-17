2fd594488b6cad57ef7ca4ddfde8138d0a0d0c55
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-08-17 12:09:36 +0100

    Arrays of structs

--- data structures/structs.c
+++ data structures/structs.c
@@ -16,6 +16,15 @@ typedef struct {
     bool isFullTime;
 }Student;
 
+typedef struct{
+    char brand[25];
+    char model[50];
+    int topSpeed;
+    float price;
+}Car;
+
+void printStudentInfo(Student student);
+void printCarInfo(Car car);
 
 int main() {
     
@@ -25,15 +34,25 @@ int main() {
     // to clear any garbage values that persisted from other programs
     Student student3 = {0}; // this sets all the members to 0
 
-    printf("Name: %s\n", student1.name);
-    printf("Age: %d\n", student1.age);
-    printf("GPA: %.1f\n", student1.gpa);
-    printf("Full time: %s\n", (student1.isFullTime) ? "true" : "false");
+    // We can also create arrays of structs 
 
-    printf("Name: %s\n", student2.name);
-    printf("Age: %d\n", student2.age);
-    printf("GPA: %.1f\n", student2.gpa);
-    printf("Full time: %s\n", (student2.isFullTime) ? "true" : "false");
+    Car cars[] = {{"Audi", "RS3", 155, 70000.00},
+                  {"Corvette", "C7 ZR1", 190, 95000.00},
+                  {"Porsche", "911 Carrera 4S", 191, 130000.00}};
+    int cars_length = sizeof(cars) / sizeof(cars[0]);
+
+    // we can use a for loop to print out the members of each struct 
+    for (int i = 0; i < cars_length; i++) {
+        printf("Make: %s, Model: %s, Top Speed: %d, Price: %c%.2f\n", cars[i].brand, cars[i].model, cars[i].topSpeed, 156, cars[i].price);
+    }
 
     return 0;
+}
+
+void printStudentInfo(Student student) {
+    printf("Name: %s\n", student.name);
+    printf("Age: %d\n", student.age);
+    printf("GPA: %.1f\n", student.gpa);
+    printf("Full time: %s\n", (student.isFullTime) ? "true" : "false");
+    printf("\n");
 }
