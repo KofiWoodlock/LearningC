214730dccb24fe6fe4cf17c3136ec044ecc6ac3a
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-08-22 11:55:48 +0100

    Writing to files

--- files/files.c
+++ files/files.c
@@ -1,3 +1,30 @@
 /*
 Files - key terms & code
+- strcut FILE 
+    - A struct defined in stdio.h to hold information about files 
 */
+
+#include <stdio.h>
+
+int main() {
+
+    // Writing to files
+    FILE *pFile = fopen("output.txt", "w");
+    char message[] = "Hello World!";
+
+    if (pFile == NULL) {
+        printf("Error opening file!");
+        return 1;
+    }
+
+    fprintf(pFile, "%s", message);
+    printf("File write successful.");
+
+
+    // ALWAYS CLOSE FILE AFTER USE OR YOU GET DATA LEAK
+    fclose(pFile);
+
+
+
+    return 0;
+}
