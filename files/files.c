43407bee344edfe286900f743381db48d090ad58
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-08-22 14:39:56 +0100

    Absolute paths

--- files/files.c
+++ files/files.c
@@ -6,10 +6,12 @@ Files - key terms & code
 
 #include <stdio.h>
 
+int wfile(char path[], char message[]);
+
 int main() {
 
     // Writing to files
-    FILE *pFile = fopen("output.txt", "w");
+    FILE *pFile = fopen("output.txt", "w"); // This is a relative path
     char message[] = "Hello World!";
 
     if (pFile == NULL) {
@@ -18,13 +20,27 @@ int main() {
     }
 
     fprintf(pFile, "%s", message);
-    printf("File write successful.");
-
+    printf("File write successful.\n");
 
     // ALWAYS CLOSE FILE AFTER USE OR YOU GET DATA LEAK
     fclose(pFile);
 
+    // Writing to a file using an absolute path
+    char filePath[] = "C:\\Users\\44759\\Desktop\\hello.txt";
 
+    wfile(filePath, "This is my desktop");
 
+    return 0;
+}
+
+int wfile(char path[], char message[]) {
+    FILE *pfile = fopen(path, "w");
+    if (pfile == NULL) {
+        printf("Error when loading file.\n");
+        return 1;
+    }
+
+    fprintf(pfile, "%s", message);
+    printf("File write successful.\n");
     return 0;
 }
