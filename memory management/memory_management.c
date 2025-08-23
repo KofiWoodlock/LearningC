dd925b1cb9d1b6eeaa89c04b4c1d744c82d0f30d
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-08-23 11:34:08 +0100

    C memeory management :(

---
+++ memory management/memory_management.c
@@ -0,0 +1,22 @@
+/*
+Memory management - key terms & code
+- Heap
+    - A region of memory used for dyanmic allocation
+- Stack
+    - A regions of memory used for storing local variables, function parameters
+    and return addresses; automatically managed when functions are called & 
+    return
+- Malloc
+    - A function used to allocate memory onto the heap
+- Calloc
+    -
+- Realloc
+    -
+- Free
+    - 
+*/
+
+int main() {
+
+    return 0;
+}
