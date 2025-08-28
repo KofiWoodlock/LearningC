c03d85e7e15ccf9c16b0242ee253e802c402bd4d
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-08-28 07:46:40 +0100

    Remove whitespaces

--- Data types/strings.c
+++ Data types/strings.c
@@ -5,7 +5,7 @@ Strings - Key terms & code
 - getchar()
     - Reads a single character from the stdin strean
 - EOF
-    - "End OF File"
+    - "End OF File" a constant defined in stdlib used to denote end of a file
 */
 
 #include <stdio.h>
@@ -15,6 +15,7 @@ Strings - Key terms & code
 void getlongest();
 int getline(char s[], int lim);
 void copy(char s[], char t[]);
+void rmblanks(char in[], char out[]);
 
 int main() {
     // Strings in C are represented by arrays of chars 
@@ -33,6 +34,10 @@ void printLongest() {
     char longest[MAXLINE]; // Stores longest line
 
     while (len = getline(line, MAXLINE) > 0)
+    /* We can make small tweaks to this conditional check to modify
+    the purpose of the program */
+        // if (len > 80)
+        //     printf("%s", line); This prints all lines greater than 80 characters
         if (len > max) {
             copy(line, longest);
         }
@@ -41,18 +46,45 @@ void printLongest() {
 
 }
 
-/* Reads a line and returns the string and it's length */
+/* Reads a line into s and returns it's length */
 int getline(char s[], int lim) {
 
     int c, i;
 
     // Loop until we see a newline character, get to end of file or reach max line length
-    for (i = 0; i < lim && (c = getchar() != EOF) && c != '\n'; ++i) { 
-
+    for (i = 0; i < lim && (c = getchar() != EOF) && c != '\n'; ++i) 
+        s[i] = c; // copy current line into the string (char array)
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
     }
-
-    return 0;
+    s[i] = '\0';
+    return i;
 }
 
 /* Copies string s into string t */
-void copy(char s[], char t[]) {}
+void copy(char s[], char t[]) {
+
+    int i = 0;
+    while (t[i] = s[i] != '\0')
+        ++i;
+}
+
+/* Removes all trailing whitespace, tabs and blank lines from input */
+/* Assumes only trailing whitespace, assumes no whitespace in input */
+void rmblanks(char in[], char out[]) {
+
+    int i = 0;
+    // Loop until we get to end of the line
+    while (in[i] != '\0') {
+        if (in[i] != ' ' && in[i] != '\t') {
+            out[i] = in[i];
+            i++;
+        }
+        else
+        // If we see a whitespace or tab ignore it and do not copy it
+            i++; 
+    }     
+
+}
+
