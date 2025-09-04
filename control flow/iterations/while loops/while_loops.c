08a0806e91b8284eb6cb11a3bd1e24562621c58c
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-09-04 22:41:49 +0100

    Number guessing game

--- control flow/iterations/while loops/while_loops.c
+++ control flow/iterations/while loops/while_loops.c
@@ -7,6 +7,9 @@ While loops - key code & terms
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+void guessTheNum();
 
 int main() {
 
@@ -21,10 +24,30 @@ int main() {
     int num = 0;
 
     // Do while loop
-    do {
-        printf("Please enter a number greater than 0: ");
-        scanf("%d", &num);
-    } while (num <= 0);
+    // do {
+    //     printf("Please enter a number greater than 0: ");
+    //     scanf("%d", &num);
+    // } while (num <= 0);
+
+    guessTheNum();
 
     return 0;
+}
+
+/* Small game where user has to guess the number i am thinking of */
+void guessTheNum() {
+    int num, guess;
+
+    num = 60;
+    printf("Try to guess the number i am thinking of between 1-100!\n");
+
+    while (scanf("%d", &guess) != EOF) {
+        if (guess == num) {
+            printf("Correct!\n");
+            break;
+        } else if (guess < num)
+            printf("Go higher!\n");
+        else
+            printf("Go lower!\n");
+    }
 }
