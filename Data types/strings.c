0270b3f4393067b1dff559074ea3e89c2329533d
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-09-14 16:07:15 +0100

    Exercise 2-5

--- Data types/strings.c
+++ Data types/strings.c
@@ -170,14 +170,9 @@ int any(char s1[], char s2[]) {
     int i, j;
 
     // loop through s1
-    for (i = 0; s1[i] != '\0'; i++) {
-        if (s1[i] == s2[0])
-            return i;
-        else
-            for (j = 1; s2[j] != '\0'; j++) {
-                if (s1[i] == s2[j])
-                    return i;
+    for (i = 0; s1[i] != '\0'; i++)
+        for (j = 0; s2[j] != '\0'; j++) { if (s1[i] == s2[j])
+                return i;
             }
-    }
     return -1; // No s2 chars found 
 }
