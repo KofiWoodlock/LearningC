207f7589601d33a03f1010637b3658e835f7800d
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-09-16 09:34:44 +0100

    inttostr function

--- control flow/iterations/while loops/while_loops.c
+++ control flow/iterations/while loops/while_loops.c
@@ -7,29 +7,36 @@ While loops - key code & terms
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 void guessTheNum();
+void itoa(int n, char s[]);
 
 int main() {
 
     // Regular while loop
-    // int num = 0;
-
-    // while (num <= 0) {
-    //     printf("Enter a number!\n");
-    //     scanf("%d", &num);
-    // }
-
+    printf("------ While Loops ------\n");
     int num = 0;
-
-    // Do while loop
-    // do {
-    //     printf("Please enter a number greater than 0: ");
-    //     scanf("%d", &num);
-    // } while (num <= 0);
+    while (num <= 0) {
+        printf("Enter a number!\n");
+        scanf("%d", &num);
+    }
 
     guessTheNum();
+
+    num = 0;
+
+    // Do while loop
+    printf("------ Do While Loops ------\n");
+    do {
+        printf("Please enter a number greater than 0: ");
+        scanf("%d", &num);
+    } while (num <= 0);
+
+    num = 123;
+    char buff[10] = {0}; 
+    itoa(num, buff);
+    printf("Num as decimal: %d, Num as string: %s\n", num, buff);
 
     return 0;
 }
@@ -50,4 +57,20 @@ void guessTheNum() {
         else
             printf("Go lower!\n");
     }
+}
+
+/* Converts integer n to a string */
+void itoa(int n, char s[]) {
+    int i, sign;
+
+    if ((sign = n) < 0) // Record sign
+        n = -n; // make n positive 
+    i = 0;
+    do {
+        s[i++] = n % 10 + '0'; // get next digits 
+    } while ((n /= 10) > 0); // remove digit from n
+    if (sign < 0)
+        s[i++] = '-';
+    s[i] = '\0';
+    strrev(s);
 }
